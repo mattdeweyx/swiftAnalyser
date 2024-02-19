6692731b937e7969bb8e16fec1dc45c6da7b638d
saiember <mattdeweyx@gmail.com>
2024-02-19 21:02:59 +0100

    test

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,26 +6,16 @@ import PackageDescription
 let package = Package(
     name: "swiftAnalyser",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swiftAnalyser",
-            targets: ["swiftAnalyser"]),
+        .executable(name: "swiftAnalyserApp", targets: ["swiftAnalyserApp"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package (url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.31.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.31.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "swiftAnalyser",
-            dependencies: [
-                .product(name: "SourceKittenFramework", package: "SourceKitten"),
-            ]),
-        .testTarget(
-            name: "swiftAnalyserTests",
-            dependencies: ["swiftAnalyser"]),
-        ]
+            path: "Sources/SomeSwiftPackage",
+            dependencies: ["SourceKittenFramework"]
+            )
+    ]
 )
