1830c75e0aa9d10bfd91358fceb2d4b3140954ad
saiember <mattdeweyx@gmail.com>
2024-02-19 21:16:28 +0100

    work?

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "swiftAnalyser",
-            dependencies: ["SourceKittenFramework"]),
+            dependencies: ["SourceKitten"]),
     ]
 )
