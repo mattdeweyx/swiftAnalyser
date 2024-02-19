563772c2049c5790a0fe1a1d7874c7f4fb80109b
saiember <mattdeweyx@gmail.com>
2024-02-19 19:48:01 +0100

    x

--- Sources/swiftAnalyserApp/main.swift
+++ Sources/swiftAnalyserApp/main.swift
@@ -1,33 +1,28 @@
 import Foundation
 import SourceKittenFramework
 
-func getImportModule(for expression: String) -> String? {
-    let file = File(contents: "import UIKit\nlet \(expression) = UIButton()")
+func getImportModule(for component: String) -> String? {
+    let file = File(contents: """
+        import UIKit
 
-    let arguments: [String] = ["-sdk", "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk", "-target", "x86_64-apple-ios12.0-simulator"]
+        // Your code here...
+        // For example:
+        let \(component) = UIButton()
+        """)
 
-    let offset = Int64(file.contents.utf8.count)
-
-    let request = Request.cursorInfo(file: file.path ?? "", offset: offset, arguments: arguments)
+    let request = CursorInfoRequest(file: file, offset: 0)
     let cursorInfo = try? request.send()
 
-    guard let module = cursorInfo?["key.modulename"] as? String else {
+    guard let module = cursorInfo?.importedModule else {
         return nil
-    } 
+    }
 
     return module
 }
 
-func main() {
-    // Example usage:
-    if let importModule = getImportModule(for: "myButton") {
-        print("The import module for myButton is: \(importModule)")
-    } else {
-        print("Failed to retrieve the import module.")
-    }
-}
-
-// Only execute the `main` function if this module is the main executable
-if CommandLine.arguments.contains(where: { $0 == "-run" }) {
-    main()
+// Example usage:
+if let importModule = getImportModule(for: "UIButton") {
+    print("The import module for UIButton is: \(importModule)")
+} else {
+    print("Failed to retrieve the import module.")
 }
