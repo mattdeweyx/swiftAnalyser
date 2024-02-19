994b6c3ba18d13e682af23d8b9277a5feab63c05
saiember <mattdeweyx@gmail.com>
2024-02-19 19:51:05 +0100

    with sourcekitten

--- Package.swift
+++ Package.swift
@@ -8,6 +8,11 @@ let package = Package(
     products: [
         .executable(name: "swiftAnalyserApp", targets: ["swiftAnalyserApp"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package (url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.31.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
